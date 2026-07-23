09f4ab27e9081f7b37c56911fd4f379f0fd2eb4d
Srinikkesh S <srinikkeshsen@gmail.com>
2026-07-23 06:54:33 -0400

    Restructured Code File

---
+++ WakeUp Code/src/main.cpp
@@ -0,0 +1,95 @@
+/*
+File:     main.py
+
+Author:   Srinikkesh Senthilkumar
+Date:     July 21, 2026
+License:  MIT License (see LICENSE in root directory)
+Copyright (c) 2026 Srinikkesh Senthilkumar
+
+Summary of File:
+
+  This file is the primary code for controlling the basic Arduino Uno R3 based light switch automater, 
+  which flips the switch to aid wake up at a pre-specified time. It integrates an LCD Module, an IR remote 
+  and receiver and an RTC Module for greater time accuracy.
+*/
+
+#include <Arduino.h>
+#include <Servo.h>
+#include <RTClib.h>
+
+
+// Pins
+const int SERVO_PIN = 11;
+
+
+// Constants
+const int SERVO_RESTING_POSITION = 180; //angle in degrees
+const int SERVO_LOCK_POSITION = 50; //angle in degrees
+const int SERVO_ACTIVE_POSITION = 0; //angle in degrees
+
+const int switchHour = 4;
+const int switchMinute = 30;
+
+const int unlockHour = 6;
+const int unlockMinute = 0;
+
+
+// Object Declarations
+Servo lightServo;
+
+RTC_DS1307 rtc;
+
+bool lock = false;
+
+ 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  
+  lightServo.attach(SERVO_PIN);
+  lightServo.write(SERVO_RESTING_POSITION);
+
+  if(! rtc.begin()){
+    Serial.println("Could not find RTC");
+    Serial.flush();
+    while(1);
+  }
+
+  // DateTime compileTime = DateTime(F(__DATE__), F(__TIME__));
+  // rtc.adjust(compileTime + TimeSpan(0, 0, 0, 5)); //Its for the consistent offset that happens when setting rtc
+
+  delay(500);
+
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  while(lock){
+    lightServo.write(SERVO_LOCK_POSITION);
+  }
+
+  if((now.hour() == switchHour) && (now.minute() == switchMinute)){
+    lightServo.write(SERVO_ACTIVE_POSITION);
+    delay(3000);
+    lightServo.write(SERVO_RESTING_POSITION);
+    lock = true;
+  };
+
+  if((now.hour() == unlockHour) && (now.minute() == unlockMinute)){
+    lock = false;
+  };
+
+  lightServo.write(SERVO_RESTING_POSITION);
+
+  delay(100); 
+}
+
+
+// Function Definitions
